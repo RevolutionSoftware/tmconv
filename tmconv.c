f087c596c9ae7bc2f59e6c06093123c6fa90ae23
Andreas Wilfer <pawilfer@protonmail.com>
2021-02-23 23:10:19 +0100

    Fleshed out main a little, added error

--- tmconv.c
+++ tmconv.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 
+int
+error(char *msg, const char *err)
+{
+	printf("Error %s: %s\n", err, msg);
+	return 1;
+}
 
 int
-main(int args, char *argv[])
+main(int argc, char *argv[])
 {
+	FILE *fp;
+	char *outnam; /* Base for output-filename */
+
+	if(argc < 2) {
+		printf("Usage: %s formated.txt [optional base for outputnames]\n", argv[0]);
+		return 0;
+	}
+	if(!(fp = fopen(argv[1], "r")))
+		return error("Cannot open file", "Invalid inputfile");
+
+	outnam = (argc >= 3) ? argv[2] : "output";
+
 	return 0;
 }
