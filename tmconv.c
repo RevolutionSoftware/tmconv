e3c3cfb5bcc551bb4325edbaf1644dc0bba42a20
Andreas Wilfer <pawilfer@protonmail.com>
2021-02-24 09:22:39 +0100

    Confusing call to error, fixed

--- tmconv.c
+++ tmconv.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
 
+typedef unsigned char Uint8;
+
+typedef struct {
+	char name[256];
+	Uint8 *data;
+} Document;
+
 int
 error(char *msg, const char *err)
 {
-	printf("Error %s: %s\n", err, msg);
+	printf("Error %s: %s\n", msg, err);
 	return 1;
 }
 
@@ -14,11 +21,11 @@ main(int argc, char *argv[])
 	char *outnam; /* Base for output-filename */
 
 	if(argc < 2) {
-		printf("Usage: %s formated.txt [optional base for outputnames]\n", argv[0]);
+		printf("Usage: %s formated.txt [optional: base for outputnames]\n", argv[0]);
 		return 0;
 	}
 	if(!(fp = fopen(argv[1], "r")))
-		return error("Cannot open file", "Invalid inputfile");
+		return error("Invalid inputfile", "Cannot open file");
 
 	outnam = (argc >= 3) ? argv[2] : "output";
 
