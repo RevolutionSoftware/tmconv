83ca5de19a1fd7392bce672722635bb7d677fa8c
Andreas Wilfer <pawilfer@protonmail.com>
2021-02-26 17:34:29 +0100

    Replaced index stuff with a proper LUT

--- tmconv.c
+++ tmconv.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-#define NPRINTABLEASCII 96
-#define FIRSTPRINTABLE 0x20
-#define LASTPRINTABLE 0x7E
-#define ISPRINTABLE(x) ((x) >= FIRSTPRINTABLE && (x) <= LASTPRINTABLE ? 1 : 0)
-
 #define HOR 32
 #define VER 16
 #define PAD 2
@@ -17,7 +12,6 @@ typedef unsigned long Uint64;
 
 typedef struct {
 	int unsaved;
-	int lut[NPRINTABLEASCII]; /* Look Up Table */
 	char name[256];
 	Uint8 data[SZ];
 	int col, row;
@@ -25,17 +19,23 @@ typedef struct {
 
 Document doc;
 
-/* Helper Functions */
-void
-genindex(Document *d) /* Deprecating and removing in a bit */
-{
-	int i;
-	char c;
-	for(i = 0, c = FIRSTPRINTABLE; c >= FIRSTPRINTABLE && c <= LASTPRINTABLE;
-		++i, ++c)
-		d->lut[i] = c;
-}
+/* Character LUT */
+static const Uint8 clookup[96] = {
+	/* clang-format off */
+	0X20, 0X21, 0X22, 0X23, 0X24, 0X25, 0X26, 0X27, 0X28, 0X29,
+	0X2A, 0X2B, 0X2C, 0X2D, 0X2E, 0X2F, 0X30, 0X31, 0X32, 0X33,
+	0X34, 0X35, 0X36, 0X37, 0X38, 0X39, 0X3A, 0X3B, 0X3C, 0X3D,
+	0X3E, 0X3F, 0X40, 0X41, 0X42, 0X43, 0X44, 0X45, 0X46, 0X47,
+	0X48, 0X49, 0X4A, 0X4B, 0X4C, 0X4D, 0X4E, 0X4F, 0X50, 0X51,
+	0X52, 0X53, 0X54, 0X55, 0X56, 0X57, 0X58, 0X59, 0X5A, 0X5B,
+	0X5C, 0X5D, 0X5E, 0X5F, 0X60, 0X61, 0X62, 0X63, 0X64, 0X65,
+	0X66, 0X67, 0X68, 0X69, 0X6A, 0X6B, 0X6C, 0X6D, 0X6E, 0X6F,
+	0X70, 0X71, 0X72, 0X73, 0X74, 0X75, 0X76, 0X77, 0X78, 0X79,
+	0X7A, 0X7B, 0X7C, 0X7D, 0X7E
+	/* clang-format on */
+};
 
+/* Helper Functions */
 int
 error(char *msg, const char *err)
 {
@@ -66,26 +66,10 @@ makedoc(Document *d, char *name)
 {
 	int i, j;
 	for(i = 0; i < SZ; ++i) d->data[i] = 0x00;
-	for(j = 0; j < NPRINTABLEASCII; ++j) d->lut[j] = 0x00;
-	genindex(d);
 	d->unsaved = 0;
 	d->col = 0;
 	d->row = 0;
 	scpy(name, d->name, 256);
-}
-
-/* For debugging the genindex (which will be depracated/removed in a bit) */
-void
-printindex()
-{
-	int i;
-	char c;
-	c = FIRSTPRINTABLE;
-	i = 0;
-	for(i = 0; i < NPRINTABLEASCII - 1 && c <= LASTPRINTABLE - 1;)
-		for(; i < NPRINTABLEASCII - 1 && c <= LASTPRINTABLE;
-			++i, ++c)
-			printf("(i[%01i], Character '%c')\n", i, doc.lut[i]);
 }
 
 int
